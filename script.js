70bd2790556c2faaa3099427e00b0a2b21d9aebd
Podexus23 <santon23@gmail.com>
2023-08-07 21:34:27 +0300

    feat: add other links move

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -23,8 +25,6 @@ btnsOpenModal.forEach(btn => {
   btn.addEventListener('click', openModal);
 });
 
-
-
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
@@ -34,17 +34,21 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
 btnScrollTo.addEventListener('click', e => {
   // let s1coords = section1.getBoundingClientRect();
-
   // window.scrollTo({
   //   left: s1coords.left + window.scrollX,
   //   top: s1coords.top + window.scrollY,
   //   behavior: 'smooth',
   // });
-
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//Page navigation
+const navLinksEl = document.querySelector('.nav__links');
+navLinksEl.addEventListener('click', e => {
+  if (!e.target.classList.contains('nav__link')) return;
+  e.preventDefault();
+  const id = e.target.getAttribute('href');
+  document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
 });
